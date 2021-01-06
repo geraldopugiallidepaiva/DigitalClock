429be7fc31aa5fa4f0d0265c3455599c34875785
Geraldo Lucas Pugialli de Paiva <56329723+geraldopugiallidepaiva@users.noreply.github.com>
2021-01-06 11:37:52 -0300

    Files updated

---
+++ library/lcd_shield.h
@@ -0,0 +1,31 @@
+/*
+ * lcd_shield.h
+ *
+ *  	Created on: January 5th 2021
+ *
+ *  	Universidade Federal de Minas Gerais
+ *
+ *  	Authors: 
+ *          Eduardo Villani
+ *          Geraldo Lucas Pugialli de Paiva
+ *  	Versão: 1.0.0
+ *
+ *  	Copyright 2021 - Under creative commons license
+ *  	Attribution-NonCommercial-ShareAlike 4.0 International
+ *  	(CC BY-NC-SA 4.0)
+ *
+ *  	This software is furnished "as is", without technical support, and with no
+ *  	warranty, express or implied, as to its usefulness for any purpose.
+
+ *
+ *  	This API was developed as an assignment for the subject Embedded Systems Programming 
+ *      at Universidade Federal de Minas Gerais
+ *      Prof. Ricardo de Oliveira Duarte - Department of Electronic Engineering
+ */
+
+#ifndef INC_LCD_SHIELD_H_
+#define INC_LCD_SHIELD_H_
+
+
+
+#endif /* INC_LCD_SHIELD_H_ */
